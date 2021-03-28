71eff48e549f46a4739000b4eaf8b9c2710ac68e
matheusdias20 <65796589+matheusdias20@users.noreply.github.com>
2021-03-27 21:28:15 -0300

    Update CountdownContext.tsx

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
 
     const { starNewChallenge } = useContext(ChallengesContext);
 
-    const [time, setTime] = useState(0.1 * 60);
+    const [time, setTime] = useState(0.1 * 2500);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
